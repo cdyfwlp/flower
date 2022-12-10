6e0aa73aa2e08b1c09d3ea5ebaf6886bc2d07d25
看客韧世 <lover@163.com>
2022-12-10 19:24:25 +0800

    #version0.1  增加窗体

---
+++ flowerGrowthRecordSystem/src/com/flower/file/CreateFlowerFile.java
@@ -0,0 +1,27 @@
+package com.flower.file;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @PackageName:com.flower.file
+ * @className:createFlowerFile
+ * @Description: 文件夹
+ * @author:xiao
+ * @date 2022/12/10 18:10
+ */
+public class CreateFlowerFile {
+       String path="C:\\Users\\Asus\\Desktop\\技术\\工作学习（新）\\维护发布\\樱花树成长记录";
+       //创建树成长照片文件夹
+       public  void  createFolder(){
+           File  file = new File(path,new SimpleDateFormat("yyyy-MM-dd-HH:mm:ss").format(new Date()));
+           if(!file.exists()){
+               file.mkdirs();
+           }
+       }
+       public static void main(String[] args) {
+           CreateFlowerFile f=new CreateFlowerFile();
+           f.createFolder();
+       }
+}
