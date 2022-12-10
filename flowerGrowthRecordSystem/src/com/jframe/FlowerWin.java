e59e264dc18f9261c2c48272077789ed7c3db50f
看客韧世 <lover@163.com>
2022-12-10 21:35:41 +0800

    #version0.1  增加窗体

--- flowerGrowthRecordSystem/src/com/jframe/FlowerWin.java
+++ flowerGrowthRecordSystem/src/com/jframe/FlowerWin.java
@@ -1,8 +1,8 @@
 package com.jframe;
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
 import java.awt.*;
+
 
 /**
  * @PackageName:com.jframe
@@ -11,28 +11,24 @@ import java.awt.*;
  * @author:xiao
  * @date 2022/12/10 19:39
  */
-public class FlowerWin extends JFrame{
-      public void createFlower(){
+public class FlowerWin{
+      private void createFlower(){
           JFrame jf=new JFrame("文件夹创建");
-          //大小
-          jf.setSize(600,300);
-          //可见
-          jf.setVisible(true);
-          //容器
-          Container container=jf.getContentPane();
-          //面板
-          JPanel jPanel=new JPanel();
-          container.add(jPanel);
-
           JTextField text=new JTextField();
+          text.setVisible(true);
+          text.setColumns(10);
           text.setBounds(150,100,300,30);
-          jPanel.add(text);
-
-          jPanel.add(new JButton("创建文件"))
+          jf.add(text);
+          jf.add(new JButton("创建文件"))
                   .setBounds(150,150,150,30);
-
-          jPanel.add(new JButton("创建当前时间文件"))
+          jf.add(new JButton("创建当前时间文件"))
                   .setBounds(300,150,150,30);
+          //大小
+          jf.setBounds(660,350,600,400);
+          //可见
+          jf.setLayout(null);
+          jf.setVisible(true);
+
       }
       public static void main(String[] args) {
           FlowerWin flowerWin=new FlowerWin();
